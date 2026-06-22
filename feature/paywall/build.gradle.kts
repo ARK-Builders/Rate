646eaec45dfed5703834631d61d4c4fc5cc22f49
mdrlzy <mordeniusss@gmail.com>
2026-06-23 04:53:05 +0500

    Setup paywall module

---
+++ feature/paywall/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose.compiler)
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "dev.arkbuilders.rate.feature.paywall"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        ksp {
+            arg("compose-destinations.moduleName", "paywall")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+dependencies {
+    implementation(project(":core:presentation"))
+
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.ui)
+    implementation(libs.navigation.compose)
+    implementation(libs.material3)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.constraintlayout.compose)
+
+    implementation(libs.timber)
+
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+
+    implementation(libs.orbit.compose)
+    implementation(libs.orbit.viewmodel)
+
+    implementation(libs.arrow.core)
+    implementation(libs.arrow.fx.coroutines)
+
+    implementation(libs.compose.destinations.core)
+    ksp(libs.compose.destinations.compiler)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
